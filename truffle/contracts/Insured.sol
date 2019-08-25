d8c285de9ea3809292fd5a9d682ebe3a1efca79d
Evgeny Marchenko <marchenko@smartdec.ru>
2019-08-25 08:23:23 +0300

    working kovan implementation

--- truffle/contracts/Insured.sol
+++ truffle/contracts/Insured.sol
@@ -2,31 +2,94 @@ pragma solidity ^0.5.10;
 
 import "./IDSProxy.sol";
 import "./IERC20.sol";
+import "./IExchange.sol";
 import "./Kovan.sol";
 
-contract Insured is Kovan {
-    event Saved(address DSProxy);    
 
-    function free_data(bytes32 cup, uint jam) public pure returns (bytes memory) {
-        return abi.encodeWithSignature("free(address,bytes32,uint256)", TUB, cup, jam);
+interface ITub {
+    function ink(bytes32) external view returns (uint);
+    function tab(bytes32) external view returns (uint);
+    function tag() external view returns (uint);
+    function per() external view returns (uint);
+}
+
+interface IVox {
+    function par() external view returns (uint);
+}
+
+
+contract Insured is Kovan {
+    uint constant LIQUIDATION = 150;
+    uint constant ACTIVE = 175;
+    uint constant TARGET = 200;
+    
+    uint constant FEE = 1e17; // 0.1 DAI - to buy MKR
+    uint constant REWARD = 1e18; // 1 DAI - to give to msg.sender
+
+    event Saved(address DSProxy);
+    
+    uint constant RAY = 10 ** 27;
+    function rmul(uint x, uint y) internal pure returns (uint z) {
+        z = (x*y + RAY / 2) / RAY;
+    }
+    function rdiv(uint x, uint y) internal pure returns (uint z) {
+        z = (x * RAY + y / 2) / y;
     }
     
-    address exchange;
-    function save(address target, bytes32 cup) external {
+    function toRepay(bytes32 cup) public returns (uint) {
+        ITub tub = ITub(TUB);
+        uint coll = rmul(tub.tag(),tub.ink(cup)); 
+        uint debt = rmul(IVox(VOX).par(), tub.tab(cup));
+        
+        uint ratio = coll * 100 / debt;
+        require(ratio < ACTIVE);
+        uint need = (TARGET * debt - 100 * coll) / (TARGET - 100);
+        uint can = coll - debt * LIQUIDATION / 100;
+        uint to = need < can ? need : can;
+        return rmul(tub.ink(cup), tub.per()) * to / coll; // redeem part of ink
+    }
+    
+    function withdraw(address target, bytes32 cup) public {
+        ITub tub = ITub(TUB);
+        uint coll = rmul(tub.tag(),tub.ink(cup)); 
+        uint debt = rmul(IVox(VOX).par(), tub.tab(cup));
+        
+        uint ratio = coll * 100 / debt;
+        require(ratio < ACTIVE);
+        uint need = (TARGET * debt - 100 * coll) / (TARGET - 100);
+        uint can = coll - debt * LIQUIDATION / 100;
+        uint to = need < can ? need : can;
+        uint amount = rmul(tub.ink(cup), tub.per()) * to / coll * 99 / 100; // redeem part of ink
+
+        bytes memory free_data = abi.encodeWithSignature("free(address,bytes32,uint256)", TUB, cup, amount);
+        IDSProxy(target).execute(PROXY, free_data);
+    }
+    
+    function sell() public {
+        IExchange(EXCH).ethToDai.value(address(this).balance)(1);
+    }
+    
+    
+    function repay(address target, bytes32 cup, uint amount) public {
+        bytes memory wipe_data = abi.encodeWithSignature("wipe(address,bytes32,uint256,address)", TUB, cup, amount, OTC);
+        IDSProxy(target).execute(PROXY, wipe_data);
+    }
+    
+    function save(address target, bytes32 cup) public {
         // main logic here
         
-        // how much collateral do we need?
-        
-        // free some collateral as WETH
-        IDSProxy(target).execute(PROXY, free_data(cup, 1543));
-        
+        // free some collateral as WETH (if required)
+        withdraw(target, cup); // 218,387  gas
+
         // how much eth we want to sell?
-        // uniswap.sell
-        bytes memory data = abi.encodeWithSignature("ethToDai(uint256)", address(this).balance);
-        (bool success, ) = exchange.delegatecall(data);//ethToDai()
-        require(success, "failed to sell eth for dai at the exchange");
-        
-        // target.wipe
+        sell(); // 69k gas
+
+        IERC20(SAI).approve(target, uint(-1));
+
+        uint amountDai = IERC20(SAI).balanceOf(address(this));
+        repay(target, cup, amountDai - FEE - REWARD);
+
+        IERC20(SAI).transfer(msg.sender, REWARD);
 
         emit Saved(target);
     }
--- truffle/contracts/Kovan.sol
+++ truffle/contracts/Kovan.sol
@@ -8,20 +8,17 @@ contract Kovan {
     // address internal constant PEP = 0x02998f73FAbb52282664094B0ff87741A1Ce9030;
     // address internal constant PIT = 0xbd747742B0F1f9791d3e6B85f8797A0cf4fbf10b;
     // address internal constant ADM = 0x74d41Fd874234D9beA31fF6b090Ba1D0b9Dc8785;
-    // address internal constant SAI = 0xC4375B7De8af5a38a93548eb8453a498222C4fF2;
+    address internal constant SAI = 0xC4375B7De8af5a38a93548eb8453a498222C4fF2;
     // address internal constant SIN = 0xdCDca4371bEFCeafA069Ca1e2AfD8b925b69e57b;
     address internal constant SKR = 0xf4d791139cE033Ad35DB2B2201435fAd668B1b64; // DAI
     // address internal constant DAD = 0x6a884c7af48e29a20be9ff04bdde112b5596fcee;
     // address internal constant MOM = 0x72ee9496b0867dfe5e8b280254da55e51e34d27b;
-    // address internal constant VOX = 0xbb4339c0ab5b1d9f14bd6e3426444a1e9d86a1d9;
+    address internal constant VOX = 0xBb4339c0aB5B1d9f14Bd6e3426444A1e9d86A1d9;
     address public constant TUB = 0xa71937147b55Deb8a530C7229C442Fd3F31b7db2; // TUB
     // address internal constant TAP = 0xc936749d2d0139174ee0271bd28325074fdbc654;
     // address internal constant TOP = 0x5f00393547561da3030ebf30e52f5dc0d5d3362c;
 
     address internal constant PROXY = 0x96Fc005a8Ba82B84B11E0Ff211a2a1362f107Ef0;
-
-    address internal constant UNISWAP = 0x09cabEC1eAd1c0Ba254B09efb3EE13841712bE14; // MAINNET!
-
-    // 0x6b1d4db70000000000000000000000000000000000000000000000000de0b6b3a7640000000000000000000000000000000000000000000000000000000000005d61b9a9
-
+    address internal constant EXCH = 0xABE3111Ea7C98BEf0F748f552F64B22106A374A4;
+    address internal constant OTC = 0x4A6bC4e803c62081ffEbCc8d227B5a87a58f1F8F;
 }
